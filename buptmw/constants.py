ede1bde4798649905f2fd43e594dfd9292282f04
ZeroCore <esetting@hotmail.com>
2025-11-24 22:26:23 +0800

    fix(ucloud): login parameters update

--- buptmw/constants.py
+++ buptmw/constants.py
@@ -15,7 +15,7 @@ class UC(StrEnum):
 
 
 class UCLOUD(StrEnum):
-    LOGIN = "https://auth.bupt.edu.cn/authserver/login?service=http://ucloud.bupt.edu.cn"
+    LOGIN = "https://auth.bupt.edu.cn/authserver/login?service=https://ucloud.bupt.edu.cn"
     TOKEN = "https://apiucloud.bupt.edu.cn/ykt-basics/oauth/token"
     INFO = "https://apiucloud.bupt.edu.cn/ykt-basics/info"
     CURRENT = "https://apiucloud.bupt.edu.cn/ykt-site/base-term/current"
